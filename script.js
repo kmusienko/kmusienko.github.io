b2eaf5581e53ae9b7eb0231adfd295cebc1da711
Kostya Musienko <kmusienko97@gmail.com>
2017-05-12 23:36:58 +0300

    new template

---
+++ script.js
@@ -0,0 +1,8 @@
+var recall = $('.btn');
+var pop = $('.popup');
+var logo = $('#logo');
+recall.click(function () {
+    'use strict';
+    logo.css('visibility', 'hidden');
+    pop.css('display', 'block');
+});
